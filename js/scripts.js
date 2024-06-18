7b5c28ffff21f8862cf02eb62750ee542a8c3214
Regiane <regianemelo1995@gmail.com>
2024-06-18 18:54:28 -0300

    reordenando notas fixas

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,27 @@ const notesContainer = document.querySelector("#notes-container")
 const noteInput = document.querySelector("#note-content")
 
 const addNoteBtn = document.querySelector(".add-note")
+
 // Funções
+
+function showNotes() {
+    cleanNotes()
+
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed)
+
+        notesContainer.appendChild(noteElement)
+
+    })
+}
+
+function cleanNotes() {
+    notesContainer.replaceChildren([])
+}
+
 function addNote() {
-    
+    const notes = getNotes()
+
     const noteObject = {
         id: generateID(),
         content: noteInput.value,
@@ -16,6 +34,12 @@ function addNote() {
     const noteElement = createNote(noteObject.id, noteObject.content)
 
     notesContainer.appendChild(noteElement)
+
+    notes.push(noteObject)
+
+    saveNotes(notes)
+
+    noteInput.value = ""
 }
 
 function generateID() {
@@ -36,9 +60,52 @@ function createNote(id, content, fixed) {
 
     element.appendChild(textarea)
 
-    return element;
+    const pinIcon = document.createElement("i")
 
+    pinIcon.classList.add(...["bi", "bi-pin"])
+
+    element.appendChild(pinIcon)
+
+    if(fixed) {
+        element.classList.add("fixed")
+    }
+
+    // Eventos do elemento
+    element.querySelector(".bi-pin").addEventListener("click", () => {
+        toggleFixNote(id)
+    })
+
+
+    return element;
+}
+
+function toggleFixNote(id) {
+    const notes = getNotes()
+
+    const targetNote = notes.filter((note) => note.id === id)[0]
+
+    targetNote.fixed = !targetNote.fixed
+
+    saveNotes(notes)
+
+    showNotes()
+}
+
+// Local storage
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+
+    const orderedNotes = notes.sort((a, b) => (a.fixed > b.fixed ? -1 : 1))
+
+    return notes
+}
+
+function saveNotes(notes) {
+    localStorage.setItem("notes",JSON.stringify(notes))
 }
 
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote())
+
+// Inicialização
+showNotes()
